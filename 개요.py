c82864d0f5544a24b1fadec835b06a11b203ea11
sos0478 <148756630+sos0478@users.noreply.github.com>
2023-11-12 13:51:14 +0900

    Add files via upload

--- 개요.py
+++ 개요.py
@@ -1,10 +1,4 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-import streamlit.components.v1 as html
-from PIL import Image
-import numpy as np
-import pandas as pd
-import seaborn as sns
 from streamlit_extras.switch_page_button import switch_page
 
 page = "상황_1.py"
